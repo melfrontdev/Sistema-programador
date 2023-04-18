64c6659c9f38dd0a6ead26af5919ea82185c9fa8
Melissa Gomes <melissa.gomes@greenwave.ind>
2023-04-18 12:19:00 -0300

    first-commit

---
+++ src/styles/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    border: 1px solid red;
+    padding: 15px;
+    width: fit-content;
+`;
